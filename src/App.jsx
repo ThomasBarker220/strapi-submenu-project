40b281f8d4f16470c2ac0bd9be9829ab50ca9715
ThomasBarker220 <james.barker@duke.edu>
2023-06-15 20:34:30 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar";
+import Submenu from "./Submenu";
+import Sidebar from "./Sidebar";
+import Hero from "./Hero";
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Sidebar />
+      <Submenu />
+    </main>
+  );
+};
+export default App;
